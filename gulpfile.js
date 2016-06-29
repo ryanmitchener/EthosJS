dc33934698e05940eb7e290c961c73ebef0754cc
Ryan Mitchener <RyanMitchener@gmail.com>
2016-06-29 17:01:45 -0400

    - Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var package = require("./package.json");
+
+// Setup scripts to watch, minify, and concat
+var scripts = ["src/**/*.js"];
+var destination = "./build/";
+
+
+// Watch file changes
+var watcher = gulp.watch(scripts.concat(scripts), ["scripts"]);
+watcher.on("change", function(event) {
+    console.log("File " + event.path + " was " + event.type + ", running tasks...");
+});
+
+
+// Concatenate and minify scripts
+gulp.task("scripts", function() {
+    return gulp.src(scripts)
+        .pipe(concat(package.name + "-" + package.version + ".concat.js", {newLine: "\n\n/* -------------------- */\n\n"}))
+        .pipe(gulp.dest(destination))
+        .pipe(rename(package.name + "-" + package.version + ".min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest(destination));
+});
+
+// Default Gulp task
+gulp.task("default", ["scripts"]);
